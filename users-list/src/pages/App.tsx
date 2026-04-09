efb6ae7d1d00bb9740df2bf60f1c485ea6d6e3b6
Dev Luis <devlgsouza@gmail.com>
2026-04-09 20:53:50 -0300

    feat - finalized css search box

--- users-list/src/pages/App.tsx
+++ users-list/src/pages/App.tsx
@@ -1,39 +1,53 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useUsers } from '../hooks/UseUsers'
 import styles from './App.module.css'
 
 function Users() {
   const { users, loading, error } = useUsers()
+
   const [search, setSearch] = useState('')
+  const [debouncedSearch, setDebouncedSearch] = useState('')
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedSearch(search)
+    }, 500)
+
+    return () => clearTimeout(timer)
+  }, [search])
 
   if (loading) return <p>Carregando...</p>
   if (error) return <p>{error}</p>
 
   const filteredUsers = users.filter((user) =>
-    user.name.toLowerCase().includes(search.toLowerCase())
+    user.name.toLowerCase().includes(debouncedSearch.toLowerCase())
   )
 
   return (
     <section className={styles.interface}>
       <div className={styles.box}>
-        <h1>Busque o usuário que desejar:</h1>
         <div className={styles.search_box}>
+          <h1>Busque o usuário que desejar:</h1>
           <input
             type="text"
-            placeholder="Buscar usuário..."
+            placeholder="Digite o nome do usuário..."
             value={search}
             onChange={(e) => setSearch(e.target.value)}
+            className={styles.input}
           />
-          <button
-            >Buscar
-          </button>
         </div>
         <div className={styles.result_list}>
-          {filteredUsers.map((user) => (
-            <p key={user.id}>
-              {user.id} - {user.name}
-            </p>
-          ))}
+          {debouncedSearch.trim() === '' ? (
+            <p>...</p>
+          ) : filteredUsers.length === 0 ? (
+            <p>Nenhum usuário encontrado</p>
+          ) : (
+            filteredUsers.map((user) => (
+              <p key={user.id}>
+                {user.id} - {user.name}
+              </p>
+            ))
+          )}
         </div>
       </div>
     </section>
