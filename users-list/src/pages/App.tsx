6ad791bb6e74668ba17e637d74d1cfc18af14af4
Dev Luis <devlgsouza@gmail.com>
2026-04-09 20:14:52 -0300

    feat - create html interface

--- users-list/src/pages/App.tsx
+++ users-list/src/pages/App.tsx
@@ -1,19 +1,42 @@
+import { useState } from 'react'
 import { useUsers } from '../hooks/UseUsers'
+import styles from './App.module.css'
 
 function Users() {
   const { users, loading, error } = useUsers()
+  const [search, setSearch] = useState('')
 
   if (loading) return <p>Carregando...</p>
   if (error) return <p>{error}</p>
 
+  const filteredUsers = users.filter((user) =>
+    user.name.toLowerCase().includes(search.toLowerCase())
+  )
+
   return (
-    <div>
-      {users.map((user) => (
-        <p key={user.id}>
-          {user.id} - {user.name}
-        </p>
-      ))}
-    </div>
+    <section className={styles.interface}>
+      <div className={styles.box}>
+        <h1>Busque o usuário que desejar:</h1>
+        <div className={styles.search_box}>
+          <input
+            type="text"
+            placeholder="Buscar usuário..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <button
+            >Buscar
+          </button>
+        </div>
+        <div className={styles.result_list}>
+          {filteredUsers.map((user) => (
+            <p key={user.id}>
+              {user.id} - {user.name}
+            </p>
+          ))}
+        </div>
+      </div>
+    </section>
   )
 }
 
