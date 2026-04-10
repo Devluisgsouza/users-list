287368d4bbad26eb4ab833ffb1a45ac23b03bc9a
Dev Luis <devlgsouza@gmail.com>
2026-04-10 16:44:24 -0300

    style - change to tailwindcss

--- users-list/src/components/UseModal/UserModal.tsx
+++ users-list/src/components/UseModal/UserModal.tsx
@@ -1,5 +1,4 @@
 import type { User } from '../../types/User'
-import styles from './UserModal.module.css'
 
 interface UserModalProps {
   user: User | null
@@ -10,12 +9,15 @@ export function UserModal({ user, onClose }: UserModalProps) {
   if (!user) return null
 
   return (
-    <div className={styles.overlay} onClick={onClose}>
+    <div
+      className="fixed left-0 top-0 flex h-full w-full items-center justify-center bg-black/70 backdrop-blur-sm"
+      onClick={onClose}
+    >
       <div
-        className={styles.user_card}
+        className="w-[30em] max-h-[50em] bg-black/80 overflow-y-auto rounded-[2em] p-8 shadow-[0px_0px_20px_#9200af]"
         onClick={(e) => e.stopPropagation()}
       >
-        <h2>{user.name}</h2>
+        <h2 className="mt-0 border-b border-[#9200af] text-purple-300 text-xl pb-2">{user.name}</h2>
 
         <p><strong>ID:</strong> {user.id}</p>
         <p><strong>Username:</strong> {user.username}</p>
@@ -23,7 +25,7 @@ export function UserModal({ user, onClose }: UserModalProps) {
         <p><strong>Telefone:</strong> {user.phone}</p>
         <p><strong>Website:</strong> {user.website}</p>
 
-        <h2>Endereço</h2>
+        <h2 className="mt-4 border-b border-[#9200af] text-purple-300 text-xl text-xl pb-2">Endereço</h2>
         <p>{user.address.street}, {user.address.suite}</p>
         <p>{user.address.city} - {user.address.zipcode}</p>
         <p>
@@ -31,13 +33,18 @@ export function UserModal({ user, onClose }: UserModalProps) {
           <strong> Longitude:</strong> {user.address.geo.lng}
         </p>
 
-        <h2>Empresa</h2>
+        <h2 className="mt-4 border-b border-[#9200af] text-purple-300 text-xl pb-2">Empresa</h2>
         <p><strong>Nome:</strong> {user.company.name}</p>
         <p><strong>Slogan:</strong> {user.company.catchPhrase}</p>
         <p><strong>Bs:</strong> {user.company.bs}</p>
 
-        <div className={styles.close}>
-          <button onClick={onClose}>Fechar</button>
+        <div className="flex justify-end">
+          <button
+            onClick={onClose}
+            className="cursor-pointer rounded-md border border-white px-4 py-2"
+          >
+            Fechar
+          </button>
         </div>
       </div>
     </div>
--- users-list/src/pages/App.tsx
+++ users-list/src/pages/App.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState, useRef } from 'react'
 import { useUsers } from '../hooks/UseUsers'
-import styles from './App.module.css'
 import type { User } from '../types/User'
 import { UserModal } from '../components/UseModal/UserModal'
+import userLogo from '../assets/user.png'
 
 function App() {
   const { users, loading, error } = useUsers()
@@ -49,10 +49,13 @@ function App() {
       )
 
   return (
-    <section className={styles.interface}>
-      <div className={styles.box}>
-        <div className={styles.search_box}>
-          <h1>Busque o usuário que desejar:</h1>
+    <section className="min-h-[98vh] flex items-center justify-center bg-gradient-to-br from-gray-950 via-purple-950 to-gray-950">
+      <div className="flex h-[40%] w-[40%] flex-col items-center justify-center rounded-[2em] p-6 bg-black shadow-[0px_0px_10px_#520e5f]">
+        <div className="flex flex-[0.5] flex-col w-[60%] items-center gap-8">
+          <div className='flex gap-3 w-[100%] item-center justify-center'> 
+            <img src={userLogo} alt="Usuário" className="w-8 h-8" />
+            <h1 className='text-2xl'>Busque o usuário que desejar:</h1>
+          </div>
           <input
             type="text"
             placeholder="Digite o nome do usuário..."
@@ -61,11 +64,18 @@ function App() {
               setSearch(e.target.value)
               setShowAll(false)
             }}
+            className="h-[15%] w-[90%] rounded-[0.8em] border-none p-3 shadow-[0px_0px_10px_#9200af] transition-shadow duration-200 hover:shadow-[0px_0px_20px_#9200af] focus:outline-none bg-black"
           />
         </div>
-        <div className={styles.result_list} ref={listRef}>
+        <div
+          className="flex flex-1 min-w-[55%] flex-col items-center overflow-y-auto rounded-[1em] p-2 mt-5 max-h-[200px]"
+          ref={listRef}
+        >
           {debouncedSearch.trim() === '' && !showAll ? (
-            <button onClick={() => setShowAll(true)} >
+            <button
+              onClick={() => setShowAll(true)}
+              className="cursor-pointer rounded-[2em] mt-10 border border-[#520e5f] p-3 transition-shadow duration-200 hover:shadow-[0px_0px_20px_#9200af]"
+            >
               Mostrar todos os usuários
             </button>
           ) : filteredUsers.length === 0 ? (
@@ -75,7 +85,7 @@ function App() {
               <p
                 key={user.id}
                 onClick={() => setSelectedUser(user)}
-                className={styles.result_item}
+                className="min-w-[100%] cursor-pointer bg-black border-b border-gray-800 border-l-2 border-l-transparent p-2 transition-all duration-300 hover:bg-[#202020] hover:border-l-[#9200af]"
               >
                 {user.name} - {user.email}
               </p>
